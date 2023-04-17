c49aedc05bad4c5a144f9dcefc257a6b56f5aa8e
Ivan Guerrero [GDL] <iguerrero@compusoluciones.com>
2023-04-17 12:36:49 -0600

    server on koa

--- app.js
+++ app.js
@@ -88,6 +88,14 @@ passport.deserializeUser(async (id, done) => {
 app.use(passport.initialize());
 app.use(passport.session());
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
 
 app.use(router);
 
